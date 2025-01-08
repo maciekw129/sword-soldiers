14ef630534c2e224b958ce4a0f763617893ba9b8
Maciej Walecki <maciejwalecki99@gmail.com>
2025-01-08 18:38:20 +0100

    feat: created input number control

--- libs/shared/ui/src/lib/ui/controls/index.ts
+++ libs/shared/ui/src/lib/ui/controls/index.ts
@@ -4,3 +4,4 @@ export { InputTextComponent } from './input-text/input-text.component';
 export { SelectButtonsComponent } from './select-buttons/select-buttons.component';
 export { SelectButtonsOption } from './select-buttons/select-buttons.model';
 export { TextareaComponent } from './textarea/textarea.component';
+export { InputNumberComponent } from './input-number/input-number.component';
---
+++ libs/shared/ui/src/lib/ui/controls/input-number/input-number.component.ts
@@ -0,0 +1,28 @@
+import { BasicFormControl } from '../abstracts/basic-form-control.abstract';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ControlErrorDirective } from '../directives/control-errors.directive';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FloatLabel } from 'primeng/floatlabel';
+
+@Component({
+  selector: 'ui-input-number',
+  standalone: true,
+  templateUrl: 'input-number.component.html',
+  imports: [
+    ControlErrorDirective,
+    ReactiveFormsModule,
+    InputNumberModule,
+    FloatLabel,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InputNumberComponent extends BasicFormControl<number> {
+  public readonly min = input<number>();
+  public readonly max = input<number>();
+  public readonly suffix = input('');
+  public readonly prefix = input('');
+  public readonly minFractionDigits = input<number>();
+  public readonly maxFractionDigits = input<number>();
+  public readonly showButtons = input(false);
+}
